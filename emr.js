a6e560245ae3262d2191e96763134fe28f80ce05
Joe <hellojoeshi@gmail.com>
2019-05-13 22:02:59 +0800

    initial commit with error

---
+++ emr.js
@@ -0,0 +1,85 @@
+const AWS = require('aws-sdk');
+const emr = AWS.EMR({
+  region: process.env.AWS_REGION || 'cn-northwest-1'
+});
+
+const sns = AWS.SNS({
+  region: process.env.AWS_REGION || 'cn-northwest-1'
+});
+
+const CLUSTER_ID = process.env.EMR_CLUSTER_ID || 'j-34LGYM3BOB5C7';
+const INSTANCE_GROUP_ID =  process.env.EMR_INSTANCE_GROUP_ID || 'ig-1G254PFJDW5SY';
+const INTERVAL = process.env.EMR_INTERVAL || 30;
+const timeout = process.env.EMR_TIMEOUT || 10 * 60;
+
+exports.scaleOut =  async (event, context) => {
+  let _timeout = timeout;
+
+  const params = {
+    ClusterId: CLUSTER_ID,
+    InstanceGroups: {
+      InstanceGroupId: INSTANCE_GROUP_ID,
+      InstanceCount: process.env.EMR_DISIRED_INSTANCE_COUNT || 3
+    }
+  };
+
+  // Scale out instance group
+  await emr.modifyInstanceGroups(params).promise();
+
+  // Loop to get instance group status
+  const interval = setInterval(async () => {
+    _timeout -= INTERVAL;
+    const message = `EMR cluster ${CLUSTER_ID} scale out failed`;
+
+    if (_timeout < 0) {
+      const snsParams = {
+        TopicArn: process.env.TOPIC_ARN || 'arn:aws-cn:sns:cn-northwest-1:057005827724:EMR',
+        Message: message
+      };
+
+      await sns.publish(snsParams).promise();
+      clearInterval(interval);
+      throw new Error(message);
+    }
+
+    const status = await getInstanceGroupStatus(CLUSTER_ID, INSTANCE_GROUP_ID);
+
+    if (status === 'RUNNING') {
+       clearInterval(interval);
+       return 'Scale out finished';
+    }
+
+  }, INTERVAL * 1000);
+
+};
+
+
+exports.scaleIn = async (event, context) => {
+
+  const params = {
+    ClusterId: CLUSTER_ID,
+    InstanceGroups: {
+      InstanceGroupId: INSTANCE_GROUP_ID,
+      InstanceCount: process.env.EMR_INITIAL_INSTANCE_COUNT || 1
+    }
+  };
+
+  return await emr.modifyInstanceGroups(params).promise();
+
+};
+
+
+/**
+ * Get the instance Group status by cluster Id and groupID
+ * @param clusterId
+ * @param groupId
+ * @returns {String}
+ */
+async function getInstanceGroupStatus(clusterId, groupId) {
+
+  const groupsData = await emr.listInstanceGroups({ ClusterId: clusterId }).promise();
+
+  const instanceGroup = groupsData.InstanceGroups.find(group => group.Id === groupId );
+
+  return instanceGroup.Status.State
+}
